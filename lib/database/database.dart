410348a949f316fe46aaa7aa850c886726b69bc3
John Requio <requ0005@algonquinlive.com>
2024-07-30 23:47:24 -0400

    Update database.dart

--- lib/database/database.dart
+++ lib/database/database.dart
@@ -4,9 +4,17 @@ import 'package:sqflite/sqflite.dart' as sqflite;
 import '../entity/flight.dart';
 import '../dao/flight_dao.dart';
 
+import 'package:final_project_flights_list/ReservationPage/BookDAO.dart';
+import 'package:final_project_flights_list/ReservationPage/BookFlight.dart';
+
 part 'database.g.dart'; // the generated code will be there
 
 @Database(version: 1, entities: [Flight])
 abstract class AppDatabase extends FloorDatabase {
   FlightDao get flightDao;
+}
+
+@Database(version: 2, entities: [BookFlight])
+abstract class ReservationDB extends FloorDatabase {
+  BookDAO get bookDao;
 }
